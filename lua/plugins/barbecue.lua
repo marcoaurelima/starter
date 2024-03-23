4a6c16fa5400163c7992ae85a5a2a4f7be98d3b2
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2024-03-22 22:49:42 -0300

    barbecue

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,13 @@
+return {
+  "utilyre/barbecue.nvim",
+  lazy = false,
+  name = "barbecue",
+  version = "*",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons", -- optional dependency
+  },
+  opts = {
+    -- configurations go here
+  },
+}
