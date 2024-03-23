d6b3b8ed7b4c663d2ab7ed8ac62491a224d0e762
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2024-03-23 00:04:48 -0300

    ajuste tamanho de terminal

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,12 +2,26 @@
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+  theme = "onedark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  term = {
+    -- hl = "Normal:term,WinSeparator:WinSeparator",
+    sizes = { sp = 0.3, vsp = 0.2 },
+    float = {
+      relative = "editor",
+      row = 0.1,
+      col = 0.1,
+      width = 0.8,
+      height = 0.7,
+      border = "single",
+    },
+    horizontal = { location = "rightbelow", split_ratio = 0.3 },
+    vertical = { location = "rightbelow", split_ratio = 0.5 },
+  },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M
